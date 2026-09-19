09d3671b0350480eda420c4908d08e53d1a4a0be
Ricter Zheng <ricterzheng@gmail.com>
2026-09-19 10:56:25 +0800

    feat: add AL1S wordmark favicon

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "al1s-wms-static-v2";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.png?v=1"];
+const CACHE = "al1s-wms-static-v3";
+const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg?v=1"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)));
